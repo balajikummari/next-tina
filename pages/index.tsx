ce150e8da95bbb2de0095d740c43806416e93a8d
Balaji Kummari <balajievlo@gmail.com>
2021-01-06 19:20:59 +0530

    genisis

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+export default function Home({ file }) {
+  const data = file.data;
   return (
     <div className={styles.container}>
       <Head>
@@ -10,12 +13,18 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className="title">
+          {/**
+           * Render the title from `home.json`
+           */}
+          - Welcome to <a href="https://nextjs.org">Next.js!</a>+ {data.title}
+        </h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +65,34 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export const getStaticProps: GetStaticProps = async function ({
+  preview,
+  previewData,
+}) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: "content/home.json",
+      parse: parseJson,
+    });
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: "content/home.json",
+        data: (await import("../content/home.json")).default,
+      },
+    },
+  };
+};
